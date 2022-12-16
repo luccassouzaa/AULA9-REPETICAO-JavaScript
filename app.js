1e3b382a0e4851438455de7f04132dc8159593e5
Luccas Andre <luccasmelim@yahoo.com.br>
2022-12-16 16:38:30 -0300

    v1.0

---
+++ app.js
@@ -0,0 +1,18 @@
+// REPETIÇÃO
+
+i = 0
+
+// WHILE
+
+while(i<10){
+    i++
+    console.log(i)
+}
+
+console.log("--------------------------------")
+
+// FOR
+
+for(i=1;i<=10;i++){
+    console.log(i)
+}
